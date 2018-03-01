481a2ce2d4bc8a8847563ccfb7eafa3117fa250b
Scott Torborg <storborg@gmail.com>
2018-03-01 14:14:37 -0800

    Some initial cleanup in sigmf-play

--- apps/sigmf_play.cc
+++ apps/sigmf_play.cc
@@ -75,18 +75,19 @@ main(int argc, char *argv[])
   // clang-format off
   main_options.add_options()
     ("help,h", "show help message")
-	("args,a", po::value<std::string>(&device_args)->default_value(""), "Argument string for usrp")
-	("wire-format", po::value<std::string>(&wire_format_str)->default_value(""), "Format of otw data")
-	("sample-rate,s", po::value<double>(&sample_rate), "Sample rate in samples/second, only used if not provided in file")
-	("center-freq,f", po::value<double>(&center_freq), "Center frequency in hertz, only used if not provided in file")
-	("gain,g", po::value<double>(&gain), "Gain in db")
-	("normalized-gain", po::value<double>(&normalized_gain), "Normalized gain")
-	("antenna", po::value<std::string>(&antenna),"Antenna for usrp")
-	("bandwidth", po::value<double>(&bandwidth), "Bandwidth for usrp")
-	("subdev-spec", po::value<std::string>(&subdev_spec), "Subdev spec for usrp")
-	("delay", po::value<double>(&delay)->default_value(0.0), "Specify a delay between repeated transmission of file")
-	("repeat", po::bool_switch(&repeat)->default_value(false), "Repeatedly transmit file")
-	("input-file", po::value<std::string>(&input_filename)->default_value(""), "File to read from");
+    ("args,a", po::value<std::string>(&device_args)->default_value(""), "Argument string for usrp")
+    ("wire-format", po::value<std::string>(&wire_format_str)->default_value(""), "Format of otw data")
+    ("sample-rate,s", po::value<double>(&sample_rate), "Sample rate in samples/second, only used if not provided in file")
+    ("freq,f", po::value<double>(&center_freq), "Center frequency in hertz, only used if not provided in file")
+    ("int-n", "Tune USRP LO in integer-N PLL mode")
+    ("gain,g", po::value<double>(&gain), "Gain in db")
+    ("normalized-gain", po::value<double>(&normalized_gain), "Normalized gain")
+    ("antenna", po::value<std::string>(&antenna),"Antenna for usrp")
+    ("bandwidth", po::value<double>(&bandwidth), "Bandwidth for usrp")
+    ("subdev-spec", po::value<std::string>(&subdev_spec), "Subdev spec for usrp")
+    ("delay", po::value<double>(&delay)->default_value(0.0), "Specify a delay between repeated transmission of file")
+    ("repeat", po::bool_switch(&repeat)->default_value(false), "Repeatedly transmit file")
+    ("input-file", po::value<std::string>(&input_filename)->default_value(""), "File to read from");
   // clang-format on
   po::positional_options_description positional_options;
   positional_options.add("input-file", 1);
@@ -100,8 +101,10 @@ main(int argc, char *argv[])
   po::notify(vm);
 
   if(vm.count("help")) {
-    std::cout << main_options << "\n";
-    return 1;
+    std::cout << "Play back a SigMF recording via a UHD device." << std::endl << std::endl;
+    std::cout << boost::format("Usage: %s [options] <filename>") % argv[0] << std::endl << std::endl;
+    std::cout << main_options << std::endl;
+    return ~0;
   }
 
   if(vm.count("gain") && vm.count("normalized-gain")) {
@@ -128,7 +131,7 @@ main(int argc, char *argv[])
   pmt::pmt_t sample_rate_pmt = file_source->global_meta().get("core:sample_rate");
   if(pmt::is_null(sample_rate_pmt)) {
     if(vm.count("sample-rate") == 0) {
-      std::cerr << "No sample rate found in metadata and none provided on command line\n";
+      std::cerr << "Error: no sample rate found in metadata and none provided on command line.\n";
       return -1;
     }
   } else {
@@ -140,14 +143,22 @@ main(int argc, char *argv[])
   pmt::pmt_t frequency_pmt =
     file_source->capture_segments().front().get("core:frequency");
   if(pmt::is_null(frequency_pmt)) {
-    if(vm.count("center_freq") == 0) {
-      std::cerr << "No frequency found in metadata and none provided on command line\n";
+    if(vm.count("freq") == 0) {
+      std::cerr << "Error: no frequency found in metadata and none provided on command line.\n";
       return -1;
     }
   } else {
     center_freq = pmt::to_double(frequency_pmt);
   }
+
+  std::cout << boost::format("Setting TX Freq: %f MHz...") % (center_freq/1e6) << std::endl;
+  uhd::tune_request_t tune_request(center_freq);
+  if(vm.count("int-n")) {
+    std::cout << "Configuring PLL in integer-N mode..." << std::endl;
+    tune_request.args = uhd::device_addr_t("mode_n=integer");
+  }
   usrp_sink->set_center_freq(center_freq);
+  std::cout << boost::format("Actual TX Freq: %f MHz...") % (usrp_sink->get_center_freq()/1e6) << std::endl << std::endl;
 
   // This gets set from the args
   if(vm.count("gain")) {
