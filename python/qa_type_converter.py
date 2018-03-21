dfb3f59fab028056e308f8f1fe70580a1c6e160c
Cate <cate@skysafe.io>
2018-03-20 17:12:25 -0700

    complex float to complex int conversion unit tests

--- python/qa_type_converter.py
+++ python/qa_type_converter.py
@@ -4,6 +4,7 @@ import shutil
 import os
 from sigmf import sigmf_swig as sigmf
 import numpy as np
+import struct
 
 
 class qa_type_converter(gr_unittest.TestCase):
@@ -19,7 +20,7 @@ class qa_type_converter(gr_unittest.TestCase):
         # Remove the directory after the test
         shutil.rmtree(self.test_dir)
 
-    def make_file(self, filename, type):
+    def make_file(self, filename, type, amplitude):
 
         filename = os.path.join(self.test_dir, filename)
 
@@ -28,29 +29,30 @@ class qa_type_converter(gr_unittest.TestCase):
         if type == "rf32":
             head = blocks.head(gr.sizeof_float * 1, self.samp_rate)
             source = analog.sig_source_f(self.samp_rate, analog.GR_COS_WAVE,
-                                         1000, 1, 0)
+                                         1000, amplitude, 0)
             tb.connect(source, head)
         elif type == "ri32":
             head = blocks.head(gr.sizeof_int * 1, self.samp_rate)
             source = analog.sig_source_i(self.samp_rate, analog.GR_COS_WAVE,
-                                         1000, 1, 0)
+                                         1000, amplitude, 0)
             tb.connect(source, head)
         elif type == "ri16":
             head = blocks.head(gr.sizeof_short * 1, self.samp_rate)
             source = analog.sig_source_s(self.samp_rate, analog.GR_COS_WAVE,
-                                         1000, 1, 0)
+                                         1000, amplitude, 0)
             tb.connect(source, head)
         elif type == "ri8":
             head = blocks.head(gr.sizeof_char * 1, self.samp_rate)
             source = analog.sig_source_f(self.samp_rate, analog.GR_COS_WAVE,
-                                         1000, 1, 0)
+                                         1000, amplitude, 0)
             convert = blocks.float_to_char(1, 1)
             tb.connect(source, convert)
             tb.connect(convert, head)
 
         else:
+            head = blocks.head(gr.sizeof_gr_complex * 1, self.samp_rate)
             source = analog.sig_source_c(self.samp_rate, analog.GR_COS_WAVE,
-                                         1000, 1, 0)
+                                         1000, amplitude, 0)
             tb.connect(source, head)
 
         sigmf_sink = sigmf.sink(type, filename)
@@ -63,7 +65,7 @@ class qa_type_converter(gr_unittest.TestCase):
 
     def test_rf32_to_ri32(self):
 
-        path = self.make_file("test_source", "rf32")
+        path = self.make_file("test_source", "rf32", 1)
 
         # expected
         expected_source = sigmf.source(path, "rf32", False, False)
@@ -91,7 +93,7 @@ class qa_type_converter(gr_unittest.TestCase):
 
     def test_rf32_to_ri16(self):
 
-        path = self.make_file("test_source", "rf32")
+        path = self.make_file("test_source", "rf32", 1)
 
         # expected
         expected_source = sigmf.source(path, "rf32", False, False)
@@ -119,7 +121,7 @@ class qa_type_converter(gr_unittest.TestCase):
 
     def test_rf32_to_ri8(self):
 
-        path = self.make_file("test_source", "rf32")
+        path = self.make_file("test_source", "rf32", 1)
 
         # expected
         expected_source = sigmf.source(path, "rf32", False, False)
@@ -150,7 +152,7 @@ class qa_type_converter(gr_unittest.TestCase):
 
     def test_ri32_to_rf32(self):
 
-        path = self.make_file("test_source", "ri32")
+        path = self.make_file("test_source", "ri32", 1)
 
         # expected
         expected_source = sigmf.source(path, "ri32", False, False)
@@ -178,7 +180,7 @@ class qa_type_converter(gr_unittest.TestCase):
 
     def test_ri32_to_ri16(self):
 
-        path = self.make_file("test_source", "ri32")
+        path = self.make_file("test_source", "ri32", 1)
 
         # expected
         expected_source = sigmf.source(path, "ri32", False, False)
@@ -204,7 +206,7 @@ class qa_type_converter(gr_unittest.TestCase):
 
     def test_ri32_to_ri8(self):
 
-        path = self.make_file("test_source", "ri32")
+        path = self.make_file("test_source", "ri32", 1)
 
         # expected
         expected_source = sigmf.source(path, "ri32", False, False)
@@ -234,7 +236,7 @@ class qa_type_converter(gr_unittest.TestCase):
 
     def test_ri16_to_rf32(self):
 
-        path = self.make_file("test_source", "ri16")
+        path = self.make_file("test_source", "ri16", 1)
 
         # expected
         expected_source = sigmf.source(path, "ri16", False, False)
@@ -262,7 +264,7 @@ class qa_type_converter(gr_unittest.TestCase):
 
     def test_ri16_to_ri32(self):
 
-        path = self.make_file("test_source", "ri16")
+        path = self.make_file("test_source", "ri16", 1)
 
         # expected
         expected_source = sigmf.source(path, "ri16", False, False)
@@ -288,7 +290,7 @@ class qa_type_converter(gr_unittest.TestCase):
 
     def test_ri16_to_ri8(self):
 
-        path = self.make_file("test_source", "ri16")
+        path = self.make_file("test_source", "ri16", 1)
 
         # expected
         expected_source = sigmf.source(path, "ri16", False, False)
@@ -316,7 +318,7 @@ class qa_type_converter(gr_unittest.TestCase):
 
     def test_ri8_to_rf32(self):
 
-        path = self.make_file("test_source", "ri8")
+        path = self.make_file("test_source", "ri8", 1)
 
         # expected
         expected_source = sigmf.source(path, "ri8", False, False)
@@ -344,7 +346,7 @@ class qa_type_converter(gr_unittest.TestCase):
 
     def test_ri8_to_ri32(self):
 
-        path = self.make_file("test_source", "ri8")
+        path = self.make_file("test_source", "ri8", 1)
 
         # expected
         expected_source = sigmf.source(path, "ri8", False, False)
@@ -374,7 +376,7 @@ class qa_type_converter(gr_unittest.TestCase):
 
     def test_ri8_to_ri16(self):
 
-        path = self.make_file("test_source", "ri8")
+        path = self.make_file("test_source", "ri8", 1)
 
         # expected
         expected_source = sigmf.source(path, "ri8", False, False)
@@ -399,3 +401,69 @@ class qa_type_converter(gr_unittest.TestCase):
         e = expected_sink.data()
         a = actual_sink.data()
         np.testing.assert_almost_equal(e, a)
+
+    def read_complex_data(self, path, dtype, size):
+        data = []
+        with open(path + ".sigmf-data", "r") as f:
+            try:
+                while True:
+                    real = struct.unpack(dtype, f.read(size))[0]
+                    imag = struct.unpack(dtype, f.read(size))[0]
+                    data.append(real + (imag * 1j))
+            except:
+                pass
+        return data
+
+    def test_cf32_to_ci32(self):
+
+        path = self.make_file("test_source", "cf32", 10)
+
+        # actual
+        filename = os.path.join(self.test_dir, "test_sink")
+        actual_source = sigmf.source(path, "ci32", False, False)
+        actual_sink = sigmf.sink("ci32", filename)
+
+        tb = gr.top_block()
+        tb.connect(actual_source, actual_sink)
+        tb.run()
+        tb.wait()
+
+        e = self.read_complex_data(path, 'f', 4)
+        a = self.read_complex_data(filename, 'i', 4)
+        np.testing.assert_almost_equal(e, a, decimal=0)
+
+    def test_cf32_to_ci16(self):
+
+        path = self.make_file("test_source", "cf32", 10)
+
+        # actual
+        filename = os.path.join(self.test_dir, "test_sink")
+        actual_source = sigmf.source(path, "ci16", False, False)
+        actual_sink = sigmf.sink("ci16", filename)
+
+        tb = gr.top_block()
+        tb.connect(actual_source, actual_sink)
+        tb.run()
+        tb.wait()
+
+        e = self.read_complex_data(path, 'f', 4)
+        a = self.read_complex_data(filename, 'h', 2)
+        np.testing.assert_almost_equal(e, a, decimal=0)
+
+    def test_cf32_to_ci8(self):
+
+        path = self.make_file("test_source", "cf32", 10)
+
+        # actual
+        filename = os.path.join(self.test_dir, "test_sink")
+        actual_source = sigmf.source(path, "ci8", False, False)
+        actual_sink = sigmf.sink("ci8", filename)
+
+        tb = gr.top_block()
+        tb.connect(actual_source, actual_sink)
+        tb.run()
+        tb.wait()
+
+        e = self.read_complex_data(path, 'f', 4)
+        a = self.read_complex_data(filename, 'b', 1)
+        np.testing.assert_almost_equal(e, a, decimal=0)
