c1a9463050b064129a7e0fa50f30fbf4bbc93921
Paul Wicks <pwicks86@gmail.com>
2018-09-17 11:24:58 -0700

    Update doxygen docs for meta_namespace

--- include/sigmf/meta_namespace.h
+++ include/sigmf/meta_namespace.h
@@ -53,20 +53,20 @@ namespace gr {
     class SIGMF_API meta_namespace {
       public:
 
-      /* \brief build meta_namespace that represents a global metadata sction
+      /*! \brief build meta_namespace that represents a global metadata sction
       * @param datatype the datatype of the file as a string
       * @param version the sigmf version of the file
       * @return the built meta_namespace object
       */
       static meta_namespace build_global_object(std::string datatype, std::string version = SIGMF_VERSION);
 
-      /* \brief build meta_namespace that represents a capture segment
+      /*! \brief build meta_namespace that represents a capture segment
       * @param sample_start the sample_start of this capture segment
       * @return the built meta_namespace object
       */
       static meta_namespace build_capture_segment(uint64_t sample_start);
 
-      /* \brief build meta_namespace that represents an annotation segment
+      /*! \brief build meta_namespace that represents an annotation segment
       * @param sample_start the sample_start of this annotation segment
       * @param sample_count the sample_count of this annotation segment
       * @return the built meta_namespace object
@@ -77,17 +77,25 @@ namespace gr {
       meta_namespace();
       ~meta_namespace();
 
-      /* \brief access the underlying pmt for this meta_namespace object
+      /*! \brief access the underlying pmt for this meta_namespace object
       * @return the pmt
       */
       pmt::pmt_t data();
 
-      /* \brief check if a given string is a valid key for SigMF metadata
+      /*! \brief check if a given string is a valid key for SigMF metadata
       * @param key the key to check
       * @return true if the key is valid and false otherwise
       */
       static bool validate_key(const std::string &key);
 
+      /*! \brief Set a value under the given key in this meta_namespace.
+      *
+      * @param key the key to use
+      * @param val value to set
+      *
+      * If the key is not in a valid format (i.e. 'namespace:name'), an
+      * std::invalid_argument exception will be thrown
+      */
       template <typename ValType>
       void
       set(const std::string &key, ValType val)
@@ -98,6 +106,8 @@ namespace gr {
         d_data = pmt::dict_add(d_data, pmt::mp(key), pmt::mp(val));
       }
 
+      /*! \copydoc set(const std::string &key, ValType val)
+      */
       void
       set(const pmt::pmt_t &key, const pmt::pmt_t &val)
       {
@@ -108,26 +118,65 @@ namespace gr {
         d_data = pmt::dict_add(d_data, key, val);
       }
 
+      /*! \copydoc set(const std::string &key, ValType val)
+      */
       // TODO: This should probably be a specialization of the template function above,
       // not an overload...
       void set(const std::string &key, pmt::pmt_t val);
 
       // TODO: refactor this with some templates...
+      /*! \brief retrieve a value under the given key
+      * @param key the key to use
+      */
       pmt::pmt_t get(const std::string &key) const;
+
+      /*! \brief retrieve a value under the given key and return a default value if it isn't found
+      * @param key the key to use
+      * @param default_val the value to use if nothing exists under the given key
+      */
       pmt::pmt_t get(const std::string &key, pmt::pmt_t default_val) const;
+
+      /*! \copydoc get(const std::string &key, pmt::pmt_t default_val) const
+      */
       pmt::pmt_t get(pmt::pmt_t key, pmt::pmt_t default_val) const;
+
+      /*! \copydoc get(const std::string &key) const;
+      */
       pmt::pmt_t get(pmt::pmt_t key) const;
+
+      /*! \copydoc data()
+      */
       pmt::pmt_t get() const;
 
+      /*! \brief retrieve a value under the given key and coerce it to a string
+      * @param key the key to use
+      */
       std::string get_str(const std::string &key) const;
 
+      /*! \brief Check if a given key exists
+      * @param key the key to check
+      * @return true if the key is present and false otherwise
+      */
       bool has(const std::string &key) const;
 
+      /*! \brief delete all data under a given key
+      * @param key the key to delete
+      */
       void del(const std::string &key);
 
+
+      /*! \brief Retrieve all keys for this object as a std::set<std::string>
+      */
       std::set<std::string> keys() const;
+
+      /*! \brief Retrieve all keys for this object as a std::set<pmt::pmt_t>
+      */
       std::set<pmt::pmt_t> pmt_keys() const;
 
+      /*! \brief Serialized this namespace to the given Writer
+      *
+      * This is used to write the namespace out to json
+      */
       template <typename Writer>
       void
       serialize(Writer &writer) const
@@ -135,6 +184,8 @@ namespace gr {
         serialize_impl(writer, d_data);
       }
 
+      /*! \brief Print a string representation of this namespace to stdout
+      */
       void print() const;
 
       private:
